c6151ee79a1e0ef848a4f7bdfaab471ff59412ed
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-11-01 23:18:42 +0800

    Disable eslint and create header layout

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Header from './app/layouts/Header';
 
 const App = () => {
-  return (
-    <Text>Hello World!</Text>
-  );
-};
+  return <Header />
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 export default App;
